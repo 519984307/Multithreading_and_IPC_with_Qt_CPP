c21d72bb7ae0e41bd720bad9c5d2b0661d67199c
CharlesKirkSD <charlesackirk@gmail.com>
2022-10-03 19:21:47 +0100

    S02_T05 MoveToThread : Complete Tutorial Implementation.
    In addition Widget::closeEvent() is implemented so that the thread is interrupted and exited before the application is closed.

---
+++ S02_CreatingThreads/S02_T05_MoveToThread/widget.h
@@ -0,0 +1,39 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+#include <QThread>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_start_pushButton_clicked();
+
+    void on_information_pushButton_clicked();
+
+    void countDone();
+    void currentCount(int value);
+    void threadFinished();
+
+private:
+    Ui::Widget *ui;
+
+    QThread * workerThread = nullptr;
+
+    // QWidget interface
+protected:
+    virtual void closeEvent(QCloseEvent *event) override;
+};
+
+#endif // WIDGET_H
---
+++ S02_CreatingThreads/S02_T05_MoveToThread/worker.cpp
@@ -0,0 +1,43 @@
+#include "worker.h"
+
+#include <QDebug>
+#include <QThread>
+#include <QVariant>
+
+Worker::Worker(QObject *parent) : QObject{parent}
+{
+    qDebug() << "Worker constructor running in thread : " << QThread::currentThread();
+}
+
+Worker::~Worker()
+{
+    qDebug() << "Worker destroyed, destructor running in thread : " << QThread::currentThread();
+}
+
+void Worker::doCounting()
+{
+    // Code in this method will run in a secondary/background thread
+    qDebug() << "Worker doCounting method running in thread : " << QThread::currentThread();
+
+    int upper = 1E8;
+    int progressUpdateSteps = 100;
+
+    for (int i{0}; i < upper; i++) {
+        double percentage = (i * 1.0 / upper) * 100;
+
+        // qDebug() << "Percentage : " << percentage;
+
+        if (i % (upper / (upper / progressUpdateSteps)) == 0) {
+            // Monday 3rd October 2022
+            // The tutorial implementation results in a crash if the user tries to quit the application before the workerThread has finished.
+            // The following if statement exits the loop if an interruption of the thread is requested.
+            // The method QThread::requestInterruption() is called in Widget::closeEvent(QCloseEvent *event) and handled in the if statement below.
+            if (thread()->isInterruptionRequested()) {
+                break;
+            }
+            emit currentCount(QVariant::fromValue(percentage).toInt());
+        }
+    }
+
+    emit countDone();
+}
